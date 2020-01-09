f911091b9676bc390bb8013a63d2ea162702fa72
diogoefl <59625433+diogoefl@users.noreply.github.com>
2020-01-09 12:34:24 -0300

    Update bigdec.erl
    Added regex validation of bitstring

--- src/bigdec.erl
+++ src/bigdec.erl
@@ -27,7 +27,7 @@
 %%=============================================================================
 %% Module setup
 %%
-%% @doc bigdec public API
+%% @doc bigdec library
 %% @end
 %%=============================================================================
 
@@ -47,6 +47,25 @@
 %% Library public API
 %%=============================================================================
 
+%% Planned API for using bigdec module
+%%
+%% => Conversions to bigdec
+%% parse(string | bitstring | float | integer)            -> #bigdec{}
+%% parse(string | bitstring | float | integer, {options}) -> #bigdec{}
+%%
+%% => Conversion from bigdec
+%% as_float(#bigdec{})            -> float
+%% as_float(#bigdec{}, {options}) -> float
+%% as_text(#bigdec{})             -> bitstring
+%% as_text(#bigdec{}, {options})  -> bitstring | string
+%%
+%% => Arithmetic
+%% add(  #bigdec{} | float | integer, #bigdec{} | float | integer)  -> #bigdec{}
+%% minus(#bigdec{} | float | integer, #bigdec{} | float | integer)  -> #bigdec{}
+%% mult( #bigdec{} | float | integer, #bigdec{} | float | integer)  -> #bigdec{}
+%% div(  #bigdec{} | float | integer, #bigdec{} | float | integer)  -> #bigdec{}
+%% pow(  #bigdec{}, integer)                                        -> #bigdec{}
+%%
 
 %%=============================================================================
 %% Internal Functions - exports for debugging during development
@@ -92,6 +111,7 @@ float_to_bitstring(FloatNum, DecimalPlaces) ->
 %% @end
 %%-----------------------------------------------------------------------------
 
+%% TODO: Segment in parts: processing integer, scientific notation and float
 bitstring_to_bigdec(Value) ->
   case binary:match(Value, <<".">>) of
 
@@ -113,6 +133,14 @@ bitstring_to_bigdec(Value) ->
     nomatch        -> erlang:error(not_implemented)
   end.
 
+-spec bitstring_processing(validate, bitstring()) -> valid | invalid.
+bitstring_processing(validate, Value) ->
+  Regexp = "[+-]?[0-9]+(\.[0-9]+)?([Ee][+-]?[0-9]+)?",
+  Length = string:length(Value),
+  case re:run(Value, Regexp) of
+    {match, [{0, Length} | _]} -> valid;
+    _                          -> invalid
+  end.
 
 %%=============================================================================
 %% Internal Functions - Analysis
@@ -121,7 +149,7 @@ bitstring_to_bigdec(Value) ->
 %%-----------------------------------------------------------------------------
 %% @doc Validates if values of bigdec has trailing zeros.
 %%
-%% Analysis bigdec value to check if it has trailing zeros. If it does return
+%% Analyze bigdec value to check if it has trailing zeros. If it does return
 %% the amount of zeros at the right of the number through a tuple:
 %%     -> {true,  AmountOfZeros}
 %%      | {false,             0}.
@@ -139,7 +167,7 @@ has_trailing_zeros(#bigdec{value = Value}) ->
 -spec has_trailing_zeros(integer(), integer()) -> integer().
 has_trailing_zeros(Value, Amount) when is_integer(Value),
                                        is_integer(Amount) ->
-  case Value rem (pow(10, Amount + 1)) == 0 of
+  case Value rem (hlp_pow(10, Amount + 1)) == 0 of
     true  -> has_trailing_zeros(Value, Amount + 1);
     false -> Amount
   end.
@@ -157,17 +185,17 @@ has_trailing_zeros(Value, Amount) when is_integer(Value),
 %% these cases it returns error.
 %% @end
 %%-----------------------------------------------------------------------------
--spec pow(integer(), non_neg_integer()) -> integer().
-pow(Number, Exponent) when is_integer(Number),
-                           is_integer(Exponent),
-                           Exponent >= 0         ->
-  pow(Number, Exponent, 1).
+-spec hlp_pow(integer(), non_neg_integer()) -> integer().
+hlp_pow(Number, Exponent) when is_integer(Number),
+                            is_integer(Exponent),
+                            Exponent >= 0         ->
+  hlp_pow(Number, Exponent, 1).
 
--spec pow(integer(), non_neg_integer(), integer()) -> integer().
-pow(  _,   0, Acc)                     -> Acc;
-pow(Num,   1, Acc)                     -> Acc*Num;
-pow(Num, Exp, Acc) when Exp rem 2 == 0 -> pow(Num*Num, Exp div 2,     Acc);
-pow(Num, Exp, Acc)                     -> pow(Num*Num, (Exp-1) div 2, Num*Acc).
+-spec hlp_pow(integer(), non_neg_integer(), integer()) -> integer().
+hlp_pow(  _,   0, Acc)                     -> Acc;
+hlp_pow(Num,   1, Acc)                     -> Acc*Num;
+hlp_pow(Num, Exp, Acc) when Exp rem 2 == 0 -> hlp_pow(Num*Num, Exp div 2,     Acc);
+hlp_pow(Num, Exp, Acc)                     -> hlp_pow(Num*Num, (Exp-1) div 2, Num*Acc).
 
 
 %%=============================================================================
@@ -176,15 +204,31 @@ pow(Num, Exp, Acc)                     -> pow(Num*Num, (Exp-1) div 2, Num*Acc).
 
 -ifdef(TEST).
 
+float_to_bitstring_test() ->
+  ?assertEqual(<<"2.20000000000000">>, float_to_bitstring(2.2)),
+  ?assertEqual(<<"12232183219874.23400000000000">>, float_to_bitstring(12232183219874.2345672345)),
+  ?assertEqual(<<"2.20">>, float_to_bitstring(2.2, 2)),
+  ?assertEqual(<<"0.30000000002000">>, float_to_bitstring(0.30000000002)).
+
+bitstring_processing_test() ->
+  ?assertEqual(valid,   bitstring_processing(validate, <<"0.1234">>)),
+  ?assertEqual(valid,   bitstring_processing(validate, <<"10238573">>)),
+  ?assertEqual(valid,   bitstring_processing(validate, <<"1E10">>)),
+  ?assertEqual(valid,   bitstring_processing(validate, <<"1223.01239842e-302">>)),
+  ?assertEqual(invalid, bitstring_processing(validate, <<"abc">>)),
+  ?assertEqual(invalid, bitstring_processing(validate, <<"j12e30">>)),
+  ?assertEqual(invalid, bitstring_processing(validate, <<"1234.3e50K">>)),
+  ?assertEqual(invalid, bitstring_processing(validate, <<"1234A.934">>)).
+
 has_trailing_zeros_test() ->
   ?assertEqual({true,  3}, has_trailing_zeros(#bigdec{value =       1000})),
   ?assertEqual({true,  3}, has_trailing_zeros(#bigdec{value =      -1000})),
   ?assertEqual({false, 0}, has_trailing_zeros(#bigdec{value = 4392345897})).
 
 pow_test() ->
-  ?assertEqual(25,                              pow(-5,   2)),
-  ?assertEqual(4261655511456885005249781170176, pow(34,  20)),
-  ?assertError(function_clause,                 pow(10, 0.5)),
-  ?assertError(function_clause,                 pow(10,  -2)).
+  ?assertEqual(25,                              hlp_pow(-5,   2)),
+  ?assertEqual(4261655511456885005249781170176, hlp_pow(34,  20)),
+  ?assertError(function_clause,                 hlp_pow(10, 0.5)),
+  ?assertError(function_clause,                 hlp_pow(10,  -2)).
 
 -endif.
